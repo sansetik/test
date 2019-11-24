485b03d5f2552886384cbc07935c74006e1c3e7c
Alex Lenez <sansetik@gmail.com>
2019-11-25 00:04:19 +0300

    handlebars version

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let express = require("express")
+let handlebars = require("express-handlebars")
 
 let app = express()
 
 app.get("/", (req, res)=>{
     res.send("ПРИВЕТ СУЧКА")
-})
 
 app.listen(3000, ()=>{
     console.log('Сервер запущен')
-})
+});
