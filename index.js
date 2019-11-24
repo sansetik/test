5db1b09e73a094abbb49e8f37b52e06735332e5c
Alex Lenez <sansetik@gmail.com>
2019-11-24 20:50:13 +0300

    version 1.0.0

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require("express")
+
+let app = express()
+
+app.get("/", (req, res)=>{
+    res.send("ПРИВЕТ СУЧКА")
+})
+
+app.listen(3000, ()=>{
+    console.log('Сервер запущен')
+})
