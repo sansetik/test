d13e7c41984fe95ab0caa04b5042bd4bd3a321ef
Alex Lenez <sansetik@gmail.com>
2019-11-30 08:27:48 +0200

    df

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 let express = require("express")
 let handlebars = require("express-handlebars")
 
+
+
 let app = express()
 
-app.get("/", (req, res)=>{
-    res.send("ПРИВЕТ СУЧКА")
+let hbs =  handlebars.create({defaultLayout: 'main', extname: "hbs"})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+ 
+app.get('/', function (req, res) {
+    res.render('home')
+})
+app.get('/about', function (req, res) {
+    res.render('about')
+})
 
 app.listen(3000, ()=>{
     console.log('Сервер запущен')
-});
+})
