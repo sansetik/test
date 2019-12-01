989cd7ee3234bf7c84c12c293d7066c99c95c753
Alex Lenez <sansetik@gmail.com>
2019-12-01 21:53:02 +0200

    hello

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-let express = require("express")
-let handlebars = require("express-handlebars")
+let express = require("express");
+let handlebars = require("express-handlebars");
 
 
 
-let app = express()
+let app = express();
+app.disable("x-powered-by") //выключаем заголовок для распознавания того чтомы пишем сайт на Express
+let hbs =  handlebars.create({defaultLayout: 'main', extname: "hbs"});
 
-let hbs =  handlebars.create({defaultLayout: 'main', extname: "hbs"})
-
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
  
 app.get('/', function (req, res) {
-    res.render('home')
-})
+    res.render('home');
+});
 app.get('/about', function (req, res) {
-    res.render('about')
-})
+    res.render('about');
+});
 
-app.listen(3000, ()=>{
-    console.log('Сервер запущен')
-})
+
+app.listen(3000, () => {
+    console.log('Сервер запущен');
+});
