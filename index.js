e07df741e7a3b4b569a5e951eedbe194d5cf7902
Alex Lenez <sansetik@gmail.com>
2019-12-05 08:32:21 +0200

    Hello

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 let express = require("express");
 let handlebars = require("express-handlebars");
-let bodyParser = require('body-parser');
-
+let bodyParser = require("body-parser");
+let formidable = require("formidable");
+let fs = require("fs");
 
 
 let app = express();
-app.disable("x-powered-by") //выключаем заголовок для распознавания того чтомы пишем сайт на Express
 let hbs =  handlebars.create({defaultLayout: 'main', extname: "hbs"});
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
+
+
+/*
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/itblog.top/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/itblog.top/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/itblog.top/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+*/
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,11 +40,16 @@ app.get('/', function (req, res) {
 app.get("/form", (req, res)=>{
     res.render("form", {});
 });
-app.post("/process", (req, res)=>{
-    
-    res.render("process", {
-        name: 1
+app.post("/process", (req, res)=>
+{
+    let form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files)=>
+    {
+        console.log("Фиелдс: " + fields);
+        console.log("this is: " + files);            
+        colsole.log(files.photo);
     });
+    
 });
 app.get('/about', function (req, res) {
     res.render('about', {
